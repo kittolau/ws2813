cb4feebdfc42f118921fc4f459f8e7f21305be01
Kitto Lau <kittolau4649@gmail.com>
2020-09-19 12:57:02 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,49 @@
-grove.onGesture(GroveGesture.Right, function () {
-    basic.showString("Hello!")
-})
-function doSomething () {
-    rainbow.clear()
-    strip.showRainbow(1, 360)
-    strip = rainbow.range(position, position + 10)
-    position = position + 1
-    if (position > 50) {
-        position = 0
+input.onButtonPressed(Button.A, function () {
+    if (ledMode == 0) {
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
+        basic.showString("W")
+        ledMode = 1
+    } else if (ledMode == 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.showString("R")
+        ledMode = 2
+    } else if (ledMode == 2) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.showString("G")
+        ledMode = 3
+    } else if (ledMode == 3) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.showString("B")
+        ledMode = 4
+    } else if (ledMode == 4) {
+        strip.showRainbow(1, 360)
+        basic.showIcon(IconNames.Heart)
+        ledMode = 0
+    } else {
+    	
     }
-}
-let position = 0
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    ledMode = 0
+    strip.showRainbow(1, 360)
+    while (!(input.buttonIsPressed(Button.A))) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    control.reset()
+})
 let strip: neopixel.Strip = null
-let rainbow: neopixel.Strip = null
-rainbow = neopixel.create(DigitalPin.P1, 300, NeoPixelMode.RGB)
+let ledMode = 0
+ledMode = 0
+let led_number = 50
+strip = neopixel.create(DigitalPin.P0, led_number, NeoPixelMode.RGB)
+strip.clear()
+strip.showColor(neopixel.rgb(0, 0, 0))
+basic.showString("Off")
 basic.forever(function () {
-    rainbow.showRainbow(1, 360)
+	
 })
